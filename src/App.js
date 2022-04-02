b8c3eb683d83c36bf564bbe156f8e53236dcc700
davidoludolapo <davidoluwadolapo@gmail.com>
2022-04-02 17:40:57 +0100

    hero banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from './Header'
+import Home from "./Home";
 
 function App() {
   return (
     <Router>  
       <Routes>
-          <Route path="/" element= {<Header />} />
+          <Route path="/" element= {<Home />} />
+          <Route path="/checkout" element= {<Header />} />
       </Routes>
     </Router>
 
